b9a923192c236c689be19f25c0e6884e2a477e0f
Megi Saputra <migisaputra06@gmail.com>
2021-05-22 14:53:55 +0700

    Praktikum 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Megi Saputra
                 </div>
 
                 <div class="links">
